bc79787a488a611ad8a04e4006a6c50931844909
Martin.Wong <wong_martin06@hotmail.com>
2024-01-20 15:18:14 +0800

    update script

--- chapter_2/Grafana/reverseProxy/environment.sh
+++ chapter_2/Grafana/reverseProxy/environment.sh
@@ -1,5 +1,5 @@
 echo "Copy nginx reverse proxy configuration file to /etc/nginx/sites-enabled/ path"
-cp nginx.conf /etc/nginx/sites-enabled/nginx.conf
+sudo cp nginx.conf /etc/nginx/sites-enabled/nginx.conf
 
 echo "Restart nginx service"
 sudo systemctl restart nginx
