6be883b37fd5be29d113beb26a871bc940919417
Martin.Wong <wong_martin06@hotmail.com>
2024-01-20 15:28:32 +0800

    update env script

--- chapter_2/Grafana/reverseProxy/environment.sh
+++ chapter_2/Grafana/reverseProxy/environment.sh
@@ -1,5 +1,15 @@
+#!/bin/bash
+
+# Ensure the script has execute permissions
+if [ -z "$EXECUTED" ]; then
+    echo "This script needs execute permissions. Attempting to update..."
+    chmod +x "$0"
+    export EXECUTED=true
+    exec "$0" "$@"
+fi
+
 echo "Copy nginx reverse proxy configuration file to /etc/nginx/sites-enabled/ path"
 sudo cp nginx.conf /etc/nginx/sites-enabled/nginx.conf
 
 echo "Restart nginx service"
-sudo systemctl restart nginx
+sudo systemctl restart nginx"
