69f725b4585039b5368b1c81a307d57849357861
Martin.Wong <wong_martin06@hotmail.com>
2024-01-20 12:07:15 +0800

    update env files

--- chapter_2/Grafana/reverseProxy/environment.sh
+++ chapter_2/Grafana/reverseProxy/environment.sh
@@ -1,15 +1,3 @@
-echo "Creating v10 data folder"
-mkdir -vp v10/data
-
-echo "Creating v9 data folder"
-mkdir -vp v9/data
-
-echo "Grant folder permission"
-chmod 777 v10
-chmod 777 v10/data
-chmod 777 v9
-chmod 777 v9/data
-
 echo "Copy nginx reverse proxy configuration file to /etc/nginx/sites-enabled/ path"
 cp nginx.conf /etc/nginx/sites-enabled/nginx.conf
 
