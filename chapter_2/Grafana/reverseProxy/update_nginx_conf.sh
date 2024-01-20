874dbd7444be114165ce1b7bc9d7831a11a79237
Martin.Wong <wong_martin06@hotmail.com>
2024-01-20 16:00:33 +0800

    combine script

--- chapter_2/Grafana/reverseProxy/update_nginx_conf.sh
+++ chapter_2/Grafana/reverseProxy/update_nginx_conf.sh
@@ -6,6 +6,9 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
+echo "Copy nginx reverse proxy configuration file to /etc/nginx/sites-enabled/ path"
+sudo cp nginx.conf /etc/nginx/sites-enabled/nginx.conf
+
 # Set the domain from the parameter
 domain="$1"
 
@@ -16,6 +19,9 @@ nginx_conf="/etc/nginx/sites-enabled/nginx.conf"  # Update with the correct path
 sed -i "s/<your_domain>/$domain/g" "$nginx_conf"
 
 echo "Nginx configuration updated with domain: $domain"
+
+echo "Restart nginx service"
+sudo systemctl restart nginx
 
 # Ensure the script has execute permissions
 chmod +x "$0"
